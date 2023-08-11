dd603599f9218d0aee22008ba56c4e458d244f71
Onoxx <leo.clarinval@icloud.com>
2023-08-11 09:26:23 +0200

    creation des formulaires

---
+++ resources/views/login.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Miam</title>
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</head>
+<body>
+    <h1>Login</h1>
+    
+    <form action="?action=login">
+        <label for="username">Nom d'utilisateur :</label>
+        <input type="text" name="username" placeholder="Username :">
+        <br>
+        <label for="password">Mot de passe :</label>
+        <input type="password" name="password" placeholder="Password :">
+    </form>
+
+</body>
+</html>
